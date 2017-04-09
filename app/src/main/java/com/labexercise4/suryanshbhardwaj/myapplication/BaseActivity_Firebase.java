ce61095b6322cc0dafade4cfc499c14971d5ad83
SURYANSH BHARDWAJ <suryanshbhardwaj46@gmail.com>
2017-04-09 17:02:35 +0530

    Create BaseActivity_Firebase.java

---
+++ app/src/main/java/com/labexercise4/suryanshbhardwaj/myapplication/BaseActivity_Firebase.java
@@ -0,0 +1,34 @@
+package com.labexercise4.suryanshbhardwaj.myapplication;
+
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity_Firebase extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("loading");
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+}
